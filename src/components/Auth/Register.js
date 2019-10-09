ccb9910d914ed5b85a0fd2612f86977da2e621b4
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-10-08 17:14:08 -0700

    refactored register using functional components

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from '../../firebase';
 import {
   Grid,
@@ -11,23 +11,29 @@ import {
 } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-class Register extends React.Component {
-  state = {
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirmation: ''
+function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const handleChange = (event, field) => {
+    if (field == 'username') {
+      setUsername(event.target.value);
+    } else if (field == 'email') {
+      setEmail(event.target.value);
+    } else if (field == 'password') {
+      setPassword(event.target.value);
+    } else if (field == 'passwordConfirmation') {
+      setPasswordConfirmation(event.target.value);
+    }
   };
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     firebase
       .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .createUserWithEmailAndPassword(email, password)
       .then(createdUser => {
         console.log(createdUser);
       })
@@ -36,69 +42,67 @@ class Register extends React.Component {
       })
   }
 
-  render() {
-    return (
-      <Grid textAlign="center" verticalAlign="middle" className="app">
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" icon color="green" textAlign="center">
-            <Icon name="puzzle piece" color="green" />
-            Register for LDSlack
-          </Header>
-          <Form onSubmit={this.handleSubmit} size="large">
-            <Segment stacked>
-              <Form.Input
-                fluid
-                name="username"
-                icon="user"
-                iconPosition="left"
-                placeholder="Username"
-                onChange={this.handleChange}
-                value={this.state.username}
-                type="text"
-              />
-              <Form.Input
-                fluid
-                name="email"
-                icon="mail"
-                iconPosition="left"
-                placeholder="Email Address"
-                onChange={this.handleChange}
-                value={this.state.email}
-                type="email"
-              />
-              <Form.Input
-                fluid
-                name="password"
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                onChange={this.handleChange}
-                value={this.state.password}
-                type="password"
-              />
-              <Form.Input
-                fluid
-                name="passwordConfirmation"
-                icon="repeat"
-                iconPosition="left"
-                placeholder="Password Confirmation"
-                onChange={this.handleChange}
-                value={this.state.passwordConfirmation}
-                type="password"
-              />
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" icon color="green" textAlign="center">
+          <Icon name="puzzle piece" color="green" />
+          Register for LDSlack
+        </Header>
+        <Form onSubmit={handleSubmit} size="large">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              name="username"
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+              onChange={event => handleChange(event, "username")}
+              value={username}
+              type="text"
+            />
+            <Form.Input
+              fluid
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email Address"
+              onChange={event => handleChange(event, "email")}
+              value={email}
+              type="email"
+            />
+            <Form.Input
+              fluid
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              onChange={event => handleChange(event, "password")}
+              value={password}
+              type="password"
+            />
+            <Form.Input
+              fluid
+              name="passwordConfirmation"
+              icon="repeat"
+              iconPosition="left"
+              placeholder="Password Confirmation"
+              onChange={event => handleChange(event, "passwordConfirmation")}
+              value={passwordConfirmation}
+              type="password"
+            />
 
-              <Button color="green" fluid size="large">
-                Submit
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
-            Already a user? <Link to="/login">Login</Link>
-          </Message>
-        </Grid.Column>
-      </Grid>
-    );
-  }
+            <Button color="green" fluid size="large">
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          Already a user? <Link to="/login">Login</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
 }
 
 export default Register;
