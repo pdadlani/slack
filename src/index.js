f54eb784d155bb78a40f40121a947e7f3f748e2a
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-10-14 21:01:26 -0700

    redirects if user already logged in.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-  <Router>
+function Root(props) {
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        props.history.push("/");
+      }
+    })
+  }, []);
+
+  return (
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Switch>
-  </Router>
-)
+  )
+} 
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
